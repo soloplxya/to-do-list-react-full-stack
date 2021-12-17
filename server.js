ca2df9c2bfc07dc9bf5411fa9bf9faadd7a584f4
soloplxya <69782911+soloplxya@users.noreply.github.com>
2021-12-18 01:51:02 +0800

    updated port to PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express(); 
 const cors = require("cors")
 const pool = require('./config/db')
-const port = process.env.port || 5000; 
+const PORT = process.env.PORT || 5000;
 const path = require("path")
 
 
@@ -82,4 +82,4 @@ app.delete("/todoes/:id", async (req, res) => {
 });
 
 // listening 
-app.listen(port, () => console.log(`Server started on ${port}.`)); 
+app.listen(PORT, () => console.log(`Server started on ${PORT}.`)); 
